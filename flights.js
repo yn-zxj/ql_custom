c0299d3357dd41203f6037f1a3ed400f50475d0f
zhangxj_a_sc <zhangxj_a_sc@si-tech.com.cn>
2023-11-24 00:31:39 +0800

    html采用模版字符串的方式

--- flights.js
+++ flights.js
@@ -7,6 +7,7 @@ const $ = new Env("机票信息查询");
 
 const axios = require('axios');
 const dayjs = require('dayjs');
+const nunjucks = require('nunjucks');
 
 /**
  * 获取access_token
@@ -104,141 +105,71 @@ async function qunarGlobalPrice(flag = false) {
 };
 
 async function main() {
-  let html = `<style>
-  .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-radius: 5px;
-    padding: 10px;
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-    margin: 5px 5px;
-    height: 70px;
-    flex-basis: calc(50% - 30px);
-    max-width: calc(50% - 30px);
-    text-decoration: none;
-  }
+  let flightInfo = {
+    oneWay: [], // 单程
+    roundTrip: [], // 往返
+    qunarLowPrice: [], // 去哪儿低价
+  };
 
-  .left {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .title {
-    font-weight: bold;
-    font-size: 17px;
-    padding-bottom: 5px;
-    color: #56004F;
-  }
-
-  .subtitle {
-    color: rgb(153, 153, 153);
-    font-size: 13px;
-  }
-
-  .right {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-  }
-
-  .discount {
-    color: rgb(153, 153, 153);
-    font-size: 10px;
-  }
-
-  .price {
-    font-size: 18px;
-    color: rgb(255, 102, 0);
-    font-weight: bold;
-  }
-
-  .currency {
-    font-size: 12px;
-    padding-right: 5px;
-  }
-
-  .separator {
-    border-top: 1px dashed rgb(239, 239, 245);
-    margin: 10px 0;
-  }
-  
-  .main-title {
-    font-weight: bold;
-    padding-left: 5px;
-    font-size: 18px;
-    color: #BE002F;
-  }
-</style>
-`;
 
   // 单程
-  qunarGlobalPriceResult = await qunarGlobalPrice();
-  html += `<div class="main-title">单程</div>`;
+  let qunarGlobalPriceResult = await qunarGlobalPrice();
+
   for (const item of qunarGlobalPriceResult.cityFlights) {
     for (const flight of item.flights) {
       console.log("航班号:%s 价格:%s(%s) 出发日期:%s 返回日期:%s", flight.carrier, flight.price, flight.tip, flight.depDate, flight.backDate);
-
-      html += `<a href="${item.allPriceSchemaUrl}" class="container" style="min-height:100px; max-width:100%">
-    <div class="left">
-      <div class="title">${qunarGlobalPriceResult.depCity} → ${flight.arrCity}</div>
-      <div class="subtitle">${flight.depDate}</div>
-    </div>
-    <div class="right">
-      <div class="discount">${flight.tip === undefined ? '' : flight.discountTip}(${flight.tip})</div>
-      <div class="price"><span class="currency">¥</span>${flight.price}</div>
-    </div>
-  </a>`;
+      flightInfo.oneWay.push({
+        schema: item.allPriceSchemaUrl,
+        depCity: qunarGlobalPriceResult.depCity,
+        arrCity: flight.arrCity,
+        tip: flight.tip,
+        discountTip: flight.discountTip,
+        depDate: flight.depDate,
+        backDate: flight.backDate,
+        carrier: flight.carrier,
+        price: flight.price
+      });
     }
-    html += `<div class="separator"></div>`;
   }
 
   // 往返
   qunarGlobalPriceResult = await qunarGlobalPrice(true);
-  html += `<div class="main-title">往返</div>`;
   for (const item of qunarGlobalPriceResult.cityFlights) {
     for (const flight of item.flights) {
       console.log("航班号:%s 价格:%s(%s) 出发日期:%s 返回日期:%s", flight.carrier, flight.price, flight.tip, flight.depDate, flight.backDate);
-
-      html += `<a href="${item.allPriceSchemaUrl}" class="container" style="min-height:100px; max-width:100%">
-    <div class="left">
-      <div class="title">${qunarGlobalPriceResult.depCity} ↔ ${flight.arrCity}</div>
-      <div class="subtitle">${flight.depDate}(去)</div>
-      <div class="subtitle">${flight.backDate}(回)</div>
-    </div>
-    <div class="right">
-      <div class="discount">${flight.tip === undefined ? '' : flight.discountTip}(${flight.tip})</div>
-      <div class="price"><span class="currency">¥</span>${flight.price}</div>
-    </div>
-  </a>`;
+      flightInfo.roundTrip.push({
+        schema: item.allPriceSchemaUrl,
+        depCity: qunarGlobalPriceResult.depCity,
+        arrCity: flight.arrCity,
+        tip: flight.tip,
+        discountTip: flight.discountTip,
+        depDate: flight.depDate,
+        backDate: flight.backDate,
+        carrier: flight.carrier,
+        price: flight.price
+      });
     }
-    html += `<div class="separator"></div>`;
   }
 
   // 低价机票
   const qunarResult = await qunar("深圳");
   for (const item of qunarResult.data.items) {
     console.log("%s", item.title);
-    html += `<div class="main-title">${item.title}</div>`;
-    html += `<div style="display: flex; flex-wrap: wrap; justify-content: center">`;
+    flightInfo.qunarLowPrice.push(item);
     for (const flight of item.flightInfoList) {
       console.log("出发地:%s 目的地:%s 航班号:%s 票价:%s(%s) 出发日期:%s(%s)", flight.depCity, flight.arrCity, flight.flightNo, flight.price, flight.discount, flight.originDepDate, flight.depWeekDay);
-      html += `<a href="${flight.scheme}" class="container">
-      <div class="left">
-        <div class="title">${flight.depCity} ${flight.backWeekDay === undefined ? '→' : '↔'}  ${flight.arrCity}</div>
-        <div class="subtitle">${flight.originDepDate} ${flight.depWeekDay}</div>
-      </div>
-      <div class="right">
-        <div class="discount">${item.title === '国内降价榜' ? `均价¥${flight.averagePrice}` : flight.discount === undefined ? '' : flight.discount}</div >
-      <div class="price"><span class="currency">¥</span>${flight.price}</div>
-      </div>
-    </a >`;
     }
-    html += `</div > <div class="separator"></div>`;
   }
 
+  // 模版路径
+  nunjucks.configure('views', {
+    autoescape: true
+  });
+
+  const html = nunjucks.render('index.html', flightInfo);
+
   // 发送的html
-  console.log('html:%s', html);
+  console.log('发送的HTML:%s', html);
 
   // 消息推送
   sendMsg('wwe758307ce630ee74', '3YrzZFoqgXdi0Xtyea8fN8-a5u8c_cWHaWsjfiXf8SM', '1000008', '机票信息', '2te06Li1BraZz2ETHA_Gpanu6Y5PwMVT_QsuP4vwH90dcMsqClrJegBnpZHVgVd8V', html, '机票助手', dayjs().format('YYYY-MM-DD HH:mm:ss') + ` 机票信息`);
